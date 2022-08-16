6e0ed8a73713b1908853f3b2380a511ffedde851
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-16 16:18:40 +0300

    Done.

---
+++ app.rb
@@ -0,0 +1,17 @@
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# puts [1,2,3]
+
+# p [1,2,3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello World!\n"
+print "Pass this test, please.\n"
+p [1,2,3]
